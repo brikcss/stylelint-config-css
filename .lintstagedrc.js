d622aa9355dd37924aa40989c9baf637759be416
Zimmee <thezimmee@gmail.com>
2019-10-23 10:42:52 -0600

    major: Update dependencies and update to v1.
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be ignored, and an empty message aborts the commit.
    #
    # On branch master
    # Your branch is up to date with 'origin/master'.
    #
    # Changes to be committed:
    #	modified:   .commitlintrc.js
    #	modified:   .lintstagedrc.js
    #	modified:   package-lock.json
    #	modified:   package.json
    #
    
    [BRANCH: master]

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,17 +1,9 @@
 /*! .lintstagedrc.js | @author Brikcss (https://github.com/brikcss) | @reference (https://github.com/okonet/lint-staged) */
 
 module.exports = {
-  linters: {
-    '*.js': ['standard --fix', 'git add'],
-    '*.json': ['prettier --parser json --write', 'git add'],
-    '*.{yml,yaml}': ['prettier --parser yaml --write', 'git add'],
-    '*.md': ['prettier --parser markdown --write', 'git add'],
-    '*.html': ['prettier --parser html --write', 'git add']
-  },
-  concurrent: true,
-  globOptions: {
-    matchBase: true,
-    dot: true
-  },
-  ignore: ['*.min.{js,css}']
+  '*.js': ['standard --fix', 'git add'],
+  '*.json': ['prettier --parser json --write', 'git add'],
+  '*.{yml,yaml}': ['prettier --parser yaml --write', 'git add'],
+  '*.md': ['prettier --parser markdown --write', 'git add'],
+  '*.html': ['prettier --parser html --write', 'git add']
 }
